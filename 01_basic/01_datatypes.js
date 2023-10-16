540a0f777f0c6c6788f596854664c3a9a6b7f776
bipulkumar254 <bipulkumar2540@yahoo.com>
2023-10-16 13:44:23 +0530

    conversion of datatypes

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,16 @@
+let isLogged = "";
+
+let booleanIsLoggedIn = Boolean(isLogged);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 0=> false
+// "" =>false 
+// "bipul" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
